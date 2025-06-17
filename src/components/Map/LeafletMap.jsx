f9872955b915dd601a8767d01d735ba99b55af64
vik782 <vincent.k.782@gmail.com>
2025-06-18 01:39:49 +1000

    Add name popup to marker

--- src/components/Map/LeafletMap.jsx
+++ src/components/Map/LeafletMap.jsx
@@ -35,6 +35,9 @@ export default function LeafletMap({ location }) {
     // Places marker of the selected location on map
     markerRef.current = L.marker(latLon).addTo(mapRef.current);
 
+    // Displays popup of location's name
+    markerRef.current.bindPopup(`<b>${location.name}</b>`).openPopup();
+
     mapRef.current.panTo(latLon); // Slowly pans to new marker
     mapRef.current.setView(latLon, 18); // Centers and zoomed into new marker
   }, [location]);
